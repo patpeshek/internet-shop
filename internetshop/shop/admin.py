7c6cfe33eb5e42d0e410b5581dfea6ac20866acf
internet-shop <nispatpeshek@gmail.com>
2024-07-09 22:16:04 +0500

    Сделал платёжку, окно платёжки и доделал отзывы

--- internetshop/shop/admin.py
+++ internetshop/shop/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Review
 
 
 class ProductAdmin(admin.ModelAdmin):
     pass
-
+class ReviewAdmin(admin.ModelAdmin):
+    pass
 
 # Register your models here.
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Review, ReviewAdmin)
--- internetshop/shop/views.py
+++ internetshop/shop/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import Product
+from .models import Product, Review
+import telebot
 
+from .config import BOT_TOKEN, CHAT_ID
+
+
+bot = telebot.TeleBot(BOT_TOKEN)
 
 # Create your views here.
 
@@ -20,6 +25,47 @@ def home(request):
     })
 def view_product(request, id):
     product = Product.objects.filter(id=id).first()
+
+    if request.method == "POST":
+        author = request.POST.get('author')
+        rating = request.POST.get('rating')
+        usage_duration = request.POST.get('duration')
+        text = request.POST.get('review')
+
+        review = Review(
+            product=product,
+            author=author,
+            rating=rating,
+            usage_duration=usage_duration,
+            text=text,
+        )
+        review.save()
+
+    reviews = product.review_set.all()
+
     return render(request, 'product.html', {
+        'product': product,
+        'reviews': reviews,
+    })
+
+def payment(request, id):
+    product = Product.objects.filter(id=id).first()
+
+    if request.method == "POST":
+        name = request.POST.get('name')
+        address = request.POST.get('address')
+        # Send message to Telegram
+        bot.send_message(CHAT_ID, f'''📦 Новый заказ: {product.name}
+💸 Цена: {product.price} рублей
+
+ФИО покупателя: {name}
+Адрес доставки: {address}
+''')
+        return redirect('/success')
+
+    return render(request, "payment.html", {
         'product': product
     })
+
+def success(request):
+    return render(request, 'success.html')
